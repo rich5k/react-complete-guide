5b8749675c9e9ef89879504377ab223c7f044ed4
rich5k <richardanatsui@gmail.com>
2020-09-28 02:11:26 +0100

    added inline styles to the switch name button

--- src/App.js
+++ src/App.js
@@ -41,10 +41,19 @@ const app= props =>{
         ]
       })
     }
+    const style={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
-          <button onClick={()=>switchNameHandler('John','Doe')}>Switch Name</button>
+          <button 
+          style={style}
+          onClick={()=>switchNameHandler('John','Doe')}>Switch Name</button>
           <Person 
             name={personsState.persons[0].name}/>
           <Person 
