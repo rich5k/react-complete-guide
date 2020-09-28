1971a9185e38053412aa19298ed09ea1659c5690
rich5k <richardanatsui@gmail.com>
2020-09-28 02:22:59 +0100

    imported userInput and UserOutput into App.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,71 @@
 import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import UserInput from "./UserInput/UserInput"
+import UserOutput from "./UserOutput/OutInput"
 
-const app= props =>{
-  const [personsState, setPersonsState]= useState({
+// Module 3 stuff
+// const app= props =>{
+//   const [personsState, setPersonsState]= useState({
     
-    persons:[
-      {name:'Max'},
-      {name: 'Manu'},
-      {name: 'Stephanie'}
-    ]
-  }
+//     persons:[
+//       {name:'Max'},
+//       {name: 'Manu'},
+//       {name: 'Stephanie'}
+//     ]
+//   }
     
-  );
+//   );
 
 
     
-  const [otherState, setOtherState]=useState('some other Value');
+//   const [otherState, setOtherState]=useState('some other Value');
 
-  console.log(personsState, otherState);
+//   console.log(personsState, otherState);
 
-  const  switchNameHandler = (newName1,newName2)=> {
-      // console.log('Was clicked!');
-      // DON'T DO THIS: this.state.persons[0].name="Richie";
-      setPersonsState({
-        persons:[
-          {name: newName1},
-          {name: newName2},
-          {name: 'Stephanie'}
-        ],
-      })
-    }
-    const nameChangedHandler= (event) =>{
-      setPersonsState({
+//   const  switchNameHandler = (newName1,newName2)=> {
+//       // console.log('Was clicked!');
+//       // DON'T DO THIS: this.state.persons[0].name="Richie";
+//       setPersonsState({
+//         persons:[
+//           {name: newName1},
+//           {name: newName2},
+//           {name: 'Stephanie'}
+//         ],
+//       })
+//     }
+//     const nameChangedHandler= (event) =>{
+//       setPersonsState({
 
-        persons:[
-          {name:'Max'},
-          {name: event.target.value},
-          {name: 'Stephanie'}
-        ]
-      })
-    }
-    const style={
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-    return (
-      <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <button 
-          style={style}
-          onClick={()=>switchNameHandler('John','Doe')}>Switch Name</button>
-          <Person 
-            name={personsState.persons[0].name}/>
-          <Person 
-            name={personsState.persons[1].name} 
-            click={switchNameHandler.bind(this,'Kwame','Fosu')}
-            changed={nameChangedHandler}>My Hobby is gaming.</Person>
-      </div>
-    );
-}
+//         persons:[
+//           {name:'Max'},
+//           {name: event.target.value},
+//           {name: 'Stephanie'}
+//         ]
+//       })
+//     }
+//     const style={
+//       backgroundColor: 'white',
+//       font: 'inherit',
+//       border: '1px solid blue',
+//       padding: '8px',
+//       cursor: 'pointer'
+//     };
+//     return (
+//       <div className="App">
+//           <h1>Hi, I'm a React App</h1>
+//           <button 
+//           style={style}
+//           onClick={()=>switchNameHandler('John','Doe')}>Switch Name</button>
+//           <Person 
+//             name={personsState.persons[0].name}/>
+//           <Person 
+//             name={personsState.persons[1].name} 
+//             click={switchNameHandler.bind(this,'Kwame','Fosu')}
+//             changed={nameChangedHandler}>My Hobby is gaming.</Person>
+//       </div>
+//     );
+// }
 
 export default app;
 
